a8c29e20259e4e1bc3025dc5e48c3ca1daa0392e
Jean Souza <jean.carlos.nmk@gmail.com>
2019-04-07 02:19:43 -0300

    Conexão com firebase adicionada no mobile

--- mobile/android/app/src/main/java/com/uaicomida/MainApplication.java
+++ mobile/android/app/src/main/java/com/uaicomida/MainApplication.java
@@ -3,6 +3,7 @@ package com.uaicomida;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new RNGestureHandlerPackage(),
             new ImagePickerPackage(),
             new VectorIconsPackage()
