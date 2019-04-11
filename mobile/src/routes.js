48196e8d7f08ccd4af3992f6308a99b6c121d0f3
unknown <guilherme.abacherli@fatec.sp.gov.br>
2019-04-10 23:49:52 -0300

    Adicionada a tela 'Table' para acessar a mesa que é selecionada na tela principal. Ajuste nas cores em 'navigationOptions'.

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -2,6 +2,7 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 import LoginScreen from './screens/Login'
 import MainScreen from './screens/Main'
+import TableScreen from './screens/Table'
 
 const MainNavigator = createStackNavigator({
     Login: {
@@ -13,17 +14,17 @@ const MainNavigator = createStackNavigator({
     Main: {
         screen: MainScreen,
         navigationOptions: {
-            title: 'uaiComida',
-            headerStyle: {
-                backgroundColor: '#ff3f34'
-            },
-            headerTintColor: '#fff'
+            title: 'Mesas',
         },
     },
+    Table: {
+        screen: TableScreen,
+    }
 }, {
         defaultNavigationOptions: {
             headerStyle: {
                 color: '#FFF',
+                backgroundColor: '#ff3f34',
             },
             headerTintColor: '#FFF',
         }
