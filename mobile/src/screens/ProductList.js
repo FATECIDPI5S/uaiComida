6663fb421c6bcf6aa276669bd89259841a7c0549
Jean Souza <jean.carlos.nmk@gmail.com>
2019-05-18 10:15:58 -0300

    Atualizando tela de produtos e mesas

--- mobile/src/screens/ProductList.js
+++ mobile/src/screens/ProductList.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
 export default class ProductList extends Component {
+
+    state = {
+        search: '',
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.text}>ProductList</Text>
+                <SearchBar
+                    placeholder='Buscar itens...'
+                    round={true}
+                    lightTheme={true}
+                    containerStyle={{backgroundColor: '#ff3f34',}}                    
+                    inputContainerStyle={{backgroundColor: '#ff3f34'}}
+                    placeholderTextColor='white'
+                    onChangeText={search => this.setState({ search })}
+                    value={this.state.search}
+                />
             </View>
         );
     }
@@ -14,8 +29,6 @@ export default class ProductList extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
     },
     text: {
         fontSize: 20,
