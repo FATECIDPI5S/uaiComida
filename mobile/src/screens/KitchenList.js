0411e06201186816297d6ab15c0c899b0054bdb1
Jean Souza <jean.carlos.nmk@gmail.com>
2019-05-22 21:22:57 -0300

    Adicionando listagem de exemplo

--- mobile/src/screens/KitchenList.js
+++ mobile/src/screens/KitchenList.js
@@ -1,12 +1,77 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, FlatList} from 'react-native';
 
 export default class KitchenList extends Component {
+
+    state = {
+        docs: [
+            {key: 'pedido 01',color: '#808080'},
+            {key: 'pedido 02',color: '#006400'},
+            {key: 'pedido 03',color: '#ffd700'},
+            {key: 'pedido 04',color: '#808080'},
+            {key: 'pedido 05',color: '#006400'},
+            {key: 'pedido 06',color: '#ffd700'},
+            {key: 'pedido 07',color: '#808080'},
+            {key: 'pedido 08',color: '#006400'},
+            {key: 'pedido 09',color: '#ffd700'},
+            {key: 'pedido 10',color: '#808080'},
+            {key: 'pedido 11',color: '#006400'},
+            {key: 'pedido 12',color: '#ffd700'},
+        ]
+    };
+
+    renderItem = ({ item }) => (
+
+        <View
+            style={{
+                flexDirection: 'row',
+                height: 50,
+                borderWidth: 1,
+                borderColor: item.color,
+                backgroundColor: item.color,
+                borderRadius: 5,
+                marginBottom: 10}}>
+
+            <View 
+                style={{
+                    flex: 0.95,
+                    justifyContent: 'center',
+                    borderRadius:5,
+                    backgroundColor:'white'}}>
+
+                <Text
+                    style={{
+                        color:'black',
+                        marginLeft:10}}>
+                    
+                    {item.key}
+                    
+                </Text>
+
+            </View>
+
+            <View
+                style={{
+                    flex: 0.05}}>
+
+            </View>
+
+        </View>
+
+    )
+
     render() {
         return (
+            
             <View style={styles.container}>
-                <Text style={styles.text}>KitchenList</Text>
+
+                <FlatList
+                    data={this.state.docs}
+                    renderItem={this.renderItem}
+                />
+
             </View>
+            
         );
     }
 }
@@ -14,10 +79,9 @@ export default class KitchenList extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        padding: 20
     },
-    text: {
-        fontSize: 20,
+    companyContainer: {
+        
     },
 });
