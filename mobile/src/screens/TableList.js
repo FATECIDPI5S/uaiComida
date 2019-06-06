21a15904d6fccad76c226c1cc350b5a2f3869f56
Jean Souza <jean.carlos.nmk@gmail.com>
2019-06-06 02:27:32 -0300

    Adicionando listagem em tempo real de mesas e ambientes

--- mobile/src/screens/TableList.js
+++ mobile/src/screens/TableList.js
@@ -1,97 +1,123 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Picker, FlatList } from 'react-native';
+import { StyleSheet, View, Text, Picker, FlatList, TouchableOpacity } from 'react-native';
 import firebase from 'react-native-firebase'
+import color from '../styles/Colors'
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class TableList extends Component {
+    constructor(){
+        super();
+        this.ambienteRef = firebase.firestore().collection('ambiente').where('ativo', '==', true);
+        this.ambienteListening = null;
+        this.mesaRef = firebase.firestore().collection('mesa').where('ativo', '==', true);
+        this.mesaListening = null;
 
-    state = {
-        ambiente: '',
-        mesas: [],
+        this.state = {
+            empresaID: '',
+            ambientes: [],
+            ambienteID: '',
+            mesas: [],            
+        };
     }
 
-    loadMesas() {
-        const mesasRef = firebase.firestore().collection('mesas');
-        mesasRef
-            .where('ativa', '==', true)
-            .orderBy('entrada', 'desc')
-            .onSnapshot(
-                querySnapshot => {
-                    const loadedMesas = [];
-                    querySnapshot
-                        .forEach(
-                            doc => {
-                                loadedMesas.push(doc.data());
-                            }
-                        )
-                    this.setState({ mesas: [] });
-                    this.setState({ mesas: loadedMesas });
+    componentDidMount() {
+        this.getUserData();
+    }
+    
+    componentWillUnmount() {
+        this.ambienteListening();
+        this.mesaListening();
+    }
 
-                }, error => { console.log(error) })  
+    async getUserData(){        
+        await firebase.firestore().collection('funcionario').doc(firebase.auth().currentUser.uid).get()
+            .then((doc) => {
+                const { empresa } = doc.data();
+                this.setState({
+                    empresaID: empresa,
+                });
+            })
+            .catch((error) => {
+                console.log('Erro ao recuperar dados do funcionário',error.message);
+            })
+        
+        this.ambienteListening = this.ambienteRef.where('empresa', '==', this.state.empresaID).onSnapshot(this.onCollectionAmbienteUpdate);
+    }
+
+    onCollectionAmbienteUpdate = (querySnapshot) => {
+        const ambientes = [];
+        querySnapshot.forEach((doc) => {
+            const { nome } = doc.data();
+
+            ambientes.push({
+                key: doc.id,
+                doc, // DocumentSnapshot
+                nome,
+            });
+        });      
+        this.setState({
+            ambientes,
+        });
+    }
+
+    onPickerValueChange = (itemValue) => {
+        this.setState({ ambienteID: itemValue })
+        this.mesaListening = this.mesaRef.where('ambiente', '==', itemValue).onSnapshot(this.onCollectionMesaUpdate);
+    }
+
+    onCollectionMesaUpdate = (querySnapshot) => {
+        const mesas = [];
+        querySnapshot.forEach((doc) => {
+            const { nome } = doc.data();
+
+            mesas.push({
+                key: doc.id,
+                doc, // DocumentSnapshot
+                nome,
+            });
+        });      
+        this.setState({
+            mesas,
+        });
     }
 
     renderMesa = ({ item }) => ( // RENDERIZA CADA ELEMENTO DA FLATLIST
         <View style={styles.mesaContainer}>
-            <TouchableOpacity style={styles.mesa} key={item.mesa} onPress={() => this.props.navigation.navigate('Table', { title: item.mesa, table: item })}>
-
+            <TouchableOpacity style={styles.mesa} key={item.nome} onPress={() => this.props.navigation.navigate('Table', { title: item.nome, table: item })}>
                 <View style={{ flexDirection: 'row' }}>
-                    <Text numberOfLines={1} style={styles.mesaTitle}>{item.mesa}</Text>
+                    <Text numberOfLines={1} style={styles.mesaTitle}>{item.nome}</Text>
                 </View>
-
                 <View style={{ flexDirection: 'row' }}>
                     <Icon style={styles.mesaIcon} name="group" color="#ff3f34" size={15} />
                     <Text style={styles.mesaContent}>Pessoas: {item.pessoas}</Text>
                 </View>
-
                 <View style={{ flexDirection: 'row' }}>
                     <Icon style={styles.mesaIcon} name="money" color="#ff3f34" size={15} />
                     <Text style={styles.mesaContent}>Valor: {item.valor}</Text>
                 </View>
-
             </TouchableOpacity>
         </View>
     )
 
-    componentWillMount() {
-        this.loadMesas();
-    }
-
     render() {
         return (
             <View style={styles.container}>
-
                 <View style={styles.pickerContainer}>
                     <Text styles={styles.pickerContainerTitle}>
                         Selecione o ambiente
                     </Text>
                     <Picker
-                        selectedValue={this.state.ambiente}
                         style={styles.picker}
-                        onValueChange={(itemValue, itemIndex) =>
-                            this.setState({ ambiente: itemValue })
-                        }>
-                        <Picker.Item label="Entrada" value="1" />
-                        <Picker.Item label="Piso superior" value="2" />
-                        <Picker.Item label="Externo" value="3" />
-                        <Picker.Item label="Externo" value="3" />
-                        <Picker.Item label="Externo" value="3" />
-                        <Picker.Item label="Externo" value="3" />
-                        <Picker.Item label="Externo" value="3" />
-                        <Picker.Item label="Externo" value="3" />
-                        <Picker.Item label="Externo" value="3" />
-                        <Picker.Item label="Externo" value="3" />
-                        <Picker.Item label="Externo" value="3" />
-                        <Picker.Item label="Externo" value="3" />
-                        <Picker.Item label="Externo" value="3" />
-                        <Picker.Item label="Externo" value="3" />
-                        <Picker.Item label="Externo" value="3" />
-                        <Picker.Item label="Externo" value="3" />
-                        <Picker.Item label="Externo" value="3" />
-                        <Picker.Item label="Externo" value="3" />
+                        selectedValue={this.state.ambienteID}
+                        onValueChange={this.onPickerValueChange}
+                    >
+                        {
+                            this.state.ambientes.map(element =>
+                                <Picker.Item label={element.nome} value={element.key} key={element.key}/>
+                            )
+                        }
                     </Picker>
                 </View>
-
-
                 <FlatList
                     numColumns={2} // Número de colunas
                     data={this.state.mesas} // AS MESAS QUE SÃO INICIADAS
@@ -99,7 +125,6 @@ export default class TableList extends Component {
                     renderItem={this.renderMesa}
                     keyExtractor={(item, index) => index.toString()} // PARA INSERIR UM KEY EM CADA COMPONENT ITEM
                 />
-
             </View>
         );
     };
@@ -111,25 +136,15 @@ const styles = StyleSheet.create({
     },
     pickerContainer: {
         height: 60,
-        backgroundColor: '#ff3f34',
+        backgroundColor: color.red,
     },
     pickerContainerTitle: {
-        fontSize: 3,
-        color: '#FFF',
+        height: 10,
+        fontSize: 53,
+        color: color.white,
     },
     picker: {
         height: 50,
-    },
-    pickerItem: {
-        height: 50,
-        width: '100%',
-        borderWidth: 1,
-        borderColor: '#ff3f34',
-        color: '#FFF',
-        borderRadius: 999,
-        backgroundColor: '#ff3f34',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
     mesaContainer: {
         flexDirection: 'row',
